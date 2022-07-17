bd446937a5a4fcd1bf7cfcba345571002348a9fb
Lihle Mqhayi <Lihle>
2022-07-17 20:30:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WebScraper
+{
+    class Program
+    {
+        private static IWebDriver driver;
+        static void Main(string[] args)
+        {
+           driver =new ChromeDriver();
+           driver.Navigate().GoToUrl("https://freek53.xyz/K53_Book_Test/bookc_lightvehicle_test.php?username=1561522910&password=2114284569&userparent=sasa");
+            var lines = driver.FindElements(By.ClassName("btext")).Select(x=>x.Text).ToArray();
+           SaveToTextFileAsync(lines, "TestCLightVehicles.txt");
+           // SaveToJasonFileAsync(lines, "TestCLightVehicles.json");
+        }
+        public static void SaveToTextFileAsync(string[] lines,string filename)
+        {
+             File.WriteAllLinesAsync(@$"C:\Users\LIhle\Desktop\{filename}", lines);
+        }
+        public static void SaveToJasonFileAsync(string[] lines, string filname)
+        {
+            string json = JsonSerializer.Serialize(lines);
+            File.WriteAllText(@$"C:\Users\LIhle\Desktop\{filname}",json);
+        }
+    }
+}
